3d4abb617b573c7f2e7a18a02db6b8889bd931ab
alexakash53 <alexakash53@gmail.com>
2022-05-07 01:08:16 +0600

    banner add

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Banner from './Pages/Home/Banner/Banner';
 import Header from './Pages/Shared/Header/Header'
 
 function App() {
   return (
     <div className="App">
         <Header></Header>
+        <Banner></Banner>
     </div>
   );
 }
